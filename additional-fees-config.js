fe43e7516af59459bc68f217f68ae1bd2c114c8b
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2023-02-09 12:53:22 -0500

    Create additional-fees-config.js

---
+++ additional-fees-config.js
@@ -0,0 +1,6 @@
+import * as ecomAdditionalFees from 'interfaces-ecommerce-v1-additional-fees-provider';
+
+/** @returns {import('interfaces-ecommerce-v1-additional-fees-provider').AdditionalFeesSPIConfig} */
+export function getConfig() {
+  return {}
+}
